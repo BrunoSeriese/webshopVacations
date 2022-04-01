85ea35c367094c8a809104de05e573342e178437
Bruno <bruno.pleysier@gmail.com>
2022-04-01 17:49:51 +0200

    try again

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/webshop-vacations'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/webshop-vacations/index.html'));});
-app.listen(process.env.PORT || 8080)
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/webshop-vacations'));
+
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/webshop-vacations/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
