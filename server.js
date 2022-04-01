cde59b470c873871f7bf98125df8b66db60da1ec
Bruno <bruno.pleysier@gmail.com>
2022-04-01 18:11:50 +0200

    updated name ... again

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/webshop-vacations'));
+app.use(express.static('./dist/wrcproject'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/webshop-vacations/'}),
+  res.sendFile('index.html', {root: 'dist/wrcproject/'}),
 );
 
 // Start the app by listening on the default Heroku port
